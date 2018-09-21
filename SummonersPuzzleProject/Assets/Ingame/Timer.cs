a6af739fd4f10a929ff1744babaeac76ad58a934
zenmaibane <koteicream.1024@gmail.com>
2018-09-21 14:30:13 +0900

    Timerの実装

--- SummonersPuzzleProject/Assets/Ingame/Timer.cs
+++ SummonersPuzzleProject/Assets/Ingame/Timer.cs
@@ -8,15 +8,57 @@ using UnityEngine;
  * 0になったら、一列目の召喚、削除と、カウントダウンのリセットをする。
  * </summary>
 */
-public class Timer : MonoBehaviour {
+public class Timer : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float summonSpeed;
+    private bool isPlaying;
+    private double countTime = 0;
+    private GameObject blockArea;
+    private GameObject HPManager;
+
+    void Start()
+    {
+        // キャラクターによって詠唱時間を変える
+        summonSpeed = 3000;
+
+        isPlaying = false;
+        blockArea = GameObject.Find("BlockArea");
+        HPManager = GameObject.Find("HPManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaying)
+        {
+            Summon(Time.deltaTime);
+        }
+    }
+
+    public void StartGame()
+    {
+        isPlaying = true;
+        countTime = 0;
+    }
+
+    private void Summon(double deltaTime)
+    {
+        countTime += deltaTime;
+        var canSummon = (countTime - summonSpeed) >= float.Epsilon;
+        if (canSummon)
+        {
+            // 最上部のモンスターを消してダメージを与える
+            GridInfo gridInfo = blockArea.GetComponent<GridInfo>();
+            int totalRank = 0;
+            for (int i = 0; i <= gridInfo.monsterPos.Length; i++)
+            {
+                totalRank += gridInfo.monsterPos[0, i].GetComponent<Block>().blockData.Rank;
+                Destroy(gridInfo.monsterPos[0, i]);
+                gridInfo.monsterPos[0, i] = null;
+            }
+            HPManager.GetComponent<HPManager>().DamageRival(totalRank);
+            countTime = 0;
+        }
+    }
 }
