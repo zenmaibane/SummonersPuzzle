1cd12ceb113893943dcd63d65d34cb9a95563bd8
zenmaibane <koteicream.1024@gmail.com>
2018-09-24 20:14:43 +0900

    キャラ変更の機構づくり

--- SummonersPuzzleProject/Assets/CharaSelect/CharaSelectSceneController.cs
+++ SummonersPuzzleProject/Assets/CharaSelect/CharaSelectSceneController.cs
@@ -11,11 +11,15 @@ public class CharaSelectSceneController : MonoBehaviour
     [SerializeField] private Button charaButton;
     [SerializeField] private CharaDataGenerator charaDataGenerator;
 
+    private GameObject characters;
+
+    private int charaIndex;
     void Start()
     {
         backButton?.onClick.AddListener(OnClickBackButton);
         selectButton?.onClick.AddListener(OnClickSelectButton);
         charaButton?.onClick.AddListener(OnClickCharaButton);
+        characters = GameObject.Find("Characters");
 
         //リリース時には消す
         SelfSceneManager.Instantiate(GameObject.Find("SceneManager"));
@@ -29,18 +33,10 @@ public class CharaSelectSceneController : MonoBehaviour
 
     void OnClickCharaButton()
     {
-        var characters = GameObject.Find("Characters");
-        var alice = characters.transform.Find("Alice");
-        var UIController = alice.gameObject.GetComponent<UIController>();
-        Debug.Log(alice);
-        if (alice.gameObject.activeSelf)
-        {
-            UIController.Hide();
-        }
-        else
-        {
-            UIController.Show();
-        }
+        charaIndex = (charaIndex + 1) % characters.transform.childCount;
+        var character = characters.transform.GetChild(charaIndex);
+        var UIController = character.gameObject.GetComponent<UIController>();
+        ChangeChara(character.gameObject.activeSelf, UIController);
     }
 
     void OnClickSelectButton()
@@ -49,5 +45,14 @@ public class CharaSelectSceneController : MonoBehaviour
             charaDataGenerator.GenerateCharaData(CharaName.Alice);
 
         SelfSceneManager.Instance.LoadBattleScene();
+    }
+
+    private void ChangeChara(bool activeSelf, UIController UIController)
+    {
+        for (var i = 0; i < characters.transform.childCount; i++)
+        {
+            characters.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        UIController.Show();
     }
 }
