e44866021773ca326f44b3c2fe78b028a4100a4b
syou4 <g1444693@gmail.com>
2018-09-26 04:29:23 +0900

    BGMの実装
    （SEも実装途中）

---
+++ SummonersPuzzleProject/Assets/Scripts/BGMManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class BGMManager : MonoBehaviour {
+
+	private AudioSource audioSource;
+	[SerializeField] private AudioClip BGM;
+	[SerializeField] private AudioClip ingameBGM;
+
+	static public BGMManager instance;
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+
+	}
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+		SceneManager.activeSceneChanged += OnActiveSceneChanged;
+
+		audioSource.Play();
+	}
+
+	// シーン遷移時に呼び出される
+	void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+	{
+		Debug.Log(prevScene.name + "->" + nextScene.name);
+
+		if (nextScene.name.Equals("Ingame"))
+		{
+			audioSource.clip = ingameBGM;
+			audioSource.Play();
+		}
+		
+		if (audioSource.clip == ingameBGM && nextScene.name.Equals("Ingame") == false)
+		{
+			audioSource.clip = BGM;
+			audioSource.Play();
+		}
+	}
+}
